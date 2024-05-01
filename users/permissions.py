bd0fc53924b0cc11993d0ed060e8d14dbba56549
antarnath <antar.ch.nath@gmail.com>
2024-05-02 01:48:50 +0600

    added sessionbase authentication

--- users/permissions.py
+++ users/permissions.py
@@ -5,10 +5,13 @@ class IsUserOwnerGetAndPostOnly(permissions.BasePermission):
     return True
   
   def has_object_permission(self, request, view, obj):
+    # show only the user's own data
     if request.method in permissions.SAFE_METHODS:
       return True
-    
+    # this data is login user's data then he can update it 
+    # obj = current user
     if not request.user.is_anonymous:
+      # check if the user is the owner of the object
       return request.user == obj
     
     return False
--- usersapp/urls.py
+++ usersapp/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import router as users_api_router
+from django.conf import settings
+from rest_framework import urls as rest_framework_urls
+
+auth_api_urls = []
+
+if settings.DEBUG:
+    auth_api_urls.append(path(r'verify/', include(rest_framework_urls))) 
 
 api_url_patterns = [
+    path(r'auth/', include(auth_api_urls)),
     path(r'accounts/', include(users_api_router.router.urls))
 ]
 
